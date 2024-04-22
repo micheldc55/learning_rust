2b2872141469afb18663fe1e58c301b1a4238345
micheldc55 <micheldc55@gmail.com>
2024-04-22 23:27:25 +0200

    added cargo build and lock

---
+++ guessing_game/target/debug/deps/guessing_game-385dbba8c2e13b1d.d
@@ -0,0 +1,5 @@
+C:\Users\miche\Desktop\Projects\learning_rust\guessing_game\target\debug\deps\libguessing_game-385dbba8c2e13b1d.rmeta: src\main.rs
+
+C:\Users\miche\Desktop\Projects\learning_rust\guessing_game\target\debug\deps\guessing_game-385dbba8c2e13b1d.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+C:\Users\miche\Desktop\Projects\learning_rust\guessing_game\target\debug\guessing_game.exe: C:\Users\miche\Desktop\Projects\learning_rust\guessing_game\src\main.rs
