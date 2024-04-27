ddc38be416912197728bb251f7e8c79fcb953950
micheldc55 <micheldc55@gmail.com>
2024-04-27 08:21:11 +0200

    guessing game cargo setup

---
+++ guessing_game/target/debug/deps/cfg_if-272a2a3ff03a99ee.d
@@ -0,0 +1,7 @@
+C:\Users\miche\Desktop\Projects\learning_rust\guessing_game\target\debug\deps\libcfg_if-272a2a3ff03a99ee.rmeta: C:\Users\miche\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\miche\Desktop\Projects\learning_rust\guessing_game\target\debug\deps\libcfg_if-272a2a3ff03a99ee.rlib: C:\Users\miche\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\miche\Desktop\Projects\learning_rust\guessing_game\target\debug\deps\cfg_if-272a2a3ff03a99ee.d: C:\Users\miche\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\miche\.cargo\registry\src\index.crates.io-6f17d22bba15001f\cfg-if-1.0.0\src\lib.rs:
