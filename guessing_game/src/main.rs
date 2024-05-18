63974636a87ee0300b771de8b1c22a9daa6542d3
micheldc55 <micheldc55@gmail.com>
2024-05-18 10:21:04 +0200

    adjusted the guessing game to output guess position vs secret num

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -17,6 +17,8 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line"); // If an error occurs, the program will crash and display the message.
 
+    let guess: u32 = guess.trim().parse().expect("Please input a number :("); // If an error occurs, the program will crash and display the message.
+
     println!("You guessed: {guess}");
 
     // Other ways to format the string
@@ -24,5 +26,9 @@ fn main() {
     // println!("x is equal to: {}", x);
     // println!("x plus two is: x + 2 = {}", x + 2);
     
-    
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Number is too small"),
+        Ordering::Equal => println!("Success!!"),
+        Ordering::Greater => println!("Number is too large"),
+    }
 }
