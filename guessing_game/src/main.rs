45bb260303bdb4d1e5767d6ef4c89d666a69877f
micheldc55 <micheldc55@gmail.com>
2024-05-23 22:50:18 +0200

    updated main to add a break when guessed correctly

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -9,26 +9,36 @@ fn main() {
 
     println!("The secret number is: {secret_number}");
 
-    println!("Please input your guess.");
+    loop {    println!("Please input your guess.");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); // If an error occurs, the program will crash and display the message.
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // If an error occurs, the program will crash and display the message.
 
-    let guess: u32 = guess.trim().parse().expect("Please input a number :("); // If an error occurs, the program will crash and display the message.
+        // trim() removes spaces and new lines (in this case the user input will have a new line character at the end, 
+        // so we need to remove it to convert the string to a number)
+        // parse() converts the string to a number
+        // expect() is used to handle errors
+        let guess: u32 = match guess.trim().parse() {
+            
+        }
 
-    println!("You guessed: {guess}");
+        println!("You guessed: {guess}");
 
-    // Other ways to format the string
-    // let x: i32 = 7;
-    // println!("x is equal to: {}", x);
-    // println!("x plus two is: x + 2 = {}", x + 2);
-    
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Number is too small"),
-        Ordering::Equal => println!("Success!!"),
-        Ordering::Greater => println!("Number is too large"),
+        // Other ways to format the string
+        // let x: i32 = 7;
+        // println!("x is equal to: {}", x);
+        // println!("x plus two is: x + 2 = {}", x + 2);
+        
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Number is too small"),
+            Ordering::Greater => println!("Number is too large"),
+            Ordering::Equal => {
+                println!("Success!!");
+                break;
+            }
+        }
     }
 }
